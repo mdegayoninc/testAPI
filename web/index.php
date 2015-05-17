a3eb2a366b222e0d334a5ad54a322cd9c64978d8
Laura <Laura@Laura-Portatil>
2015-05-17 22:46:19 +0200

    Testing routes

--- web/index.php
+++ web/index.php
@@ -8,6 +8,15 @@ $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
 });
 
+$app->get('/jarl/', function () use ($app) {
+    return 'jarl';
+});
+
+
+$app->get('/', function () use ($app) {
+    return 'Index';
+});
+
 $app->run();
 
 ?>
