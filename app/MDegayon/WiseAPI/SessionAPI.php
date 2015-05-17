d5956cd659748150cb4d2393f98da6fc4a1160cf
Laura <Laura@Laura-Portatil>
2015-05-18 01:16:29 +0200

    Added attributes, constants and some yet untested functionality

--- app/MDegayon/WiseAPI/SessionAPI.php
+++ app/MDegayon/WiseAPI/SessionAPI.php
@@ -1,12 +1,66 @@
 <?php
 namespace MDegayon\WiseAPI;
+
+use MDegayon\WiseAPI\WisemblyAPIConnection as Connection;
 /**
  * SessionAPI for logged in users
  *
  * @author Miguel Degayon
  */
-class SessionAPI {
+class SessionAPI 
+{
 
+    private $token;
+
+    const WISE_TOKEN_HEADER = 'Wisembly-Token';
+    
+    
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    public static function getUserEvents($userHash)
+    {
+        
+        $event = false;
+
+        try{
+
+            $response = Request::get( 
+                    Connection::API_ADDRESS .'/'.
+                    Connection::API_V4 . '/user/'.$eventKeyword.'/wiz')
+                ->addHeader(SessionAPI::WISE_TOKEN_HEADER, $this->token)
+                ->send(); 
+
+        }catch(Exception $e){
+            $event = false;    
+        }
+        
+        $events = $this->createEventsFromResponse($response);
+
+        return $connection;      
+        
+        
+    }
+    
+    public static function getStream()
+    {
+        
+    }
+    
+    public static function addMessageToStream()
+    {
+        
+        
+    }
+    
+    private function createEventFromResponse(Httpful\Response $response)
+    {
+        
+    }
+    
+    
 }
 
 ?>
