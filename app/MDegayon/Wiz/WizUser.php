185631ef063f21af3d17f0007452778fa0695368
Laura <Laura@Laura-Portatil>
2015-05-18 00:37:46 +0200

    Created a class for the Wisemby user

---
+++ app/MDegayon/Wiz/WizUser.php
@@ -0,0 +1,25 @@
+<?php
+namespace MDegayon\Wiz;
+
+/**
+ * Class Representing an user of Wisembly Application
+ *
+ * @author Laura
+ */
+class WizUser
+{
+    
+    private $hash,      
+            $name,
+            $email,
+            $company;
+    
+    public function __construct($hash, $name, $email,$company) 
+    {
+        $this->hash = $hash;
+        $this->name = $name;
+        $this->email = $email;
+        $this->company = $company;
+    }
+}
+?>
