b59d9771050956b463375a9fa4e82784d5b7cb80
Laura <Laura@Laura-Portatil>
2015-05-18 01:49:47 +0200

    Added session and user creation/parsing from response

--- app/MDegayon/WiseAPI/WisemblyAPIConnection.php
+++ app/MDegayon/WiseAPI/WisemblyAPIConnection.php
@@ -1,7 +1,7 @@
 <?php
 namespace MDegayon\WiseAPI;
 
-use Httpful\Request as Request;
+use \Httpful\Request as Request;
 use MDegayon\Wiz\WizUser as WizUser;
 
 
@@ -13,13 +13,17 @@ use MDegayon\Wiz\WizUser as WizUser;
 class WisemblyAPIConnection
 {
     
-    const   API_ADDRESS = 'https://api.wisembly.com',
+    const   
+            USER_KEY = 'user',
+            SESSION_API_KEY = 'api',
+            API_ADDRESS = 'https://api.wisembly.com',
             SUCCESSFUL_REQUEST_CODE = 200,
             API_V4 =   'api/4';
     
     public static function connect($email, $secret, $app_id, $hash)
     {
     
+        $response = false;
         $connection = false;
             
         $body =     '{"email" : "'.$email.
@@ -37,9 +41,19 @@ class WisemblyAPIConnection
         if ($response->code != WisemblyAPIConnection::SUCCESSFUL_REQUEST_CODE){
 
             throw new InvalidArgumentException
-                    ("Error while trying to connect to API : ");
+                    ("Error while trying to connect to the API : ");
         }
         $user = WisemblyAPIConnection::createUserFromResponse($response);
+        $sessionAPI = WisemblyAPIConnection::createSessionAPI($response);
+        
+        //TODO: Add an exceptions for each error (Couldn't create user and couldn't create API)
+        //TODO: Replace array with some CONNECTION RESPONSE OBJECT including both user and API. 
+        //(with also maybe some error messages instead of exceptions in case something went wrong
+        
+        if($user && $sessionAPI){
+            $response = array(  WisemblyAPIConnection::USER_KEY => $user,
+                                WisemblyAPIConnection::SESSION_API_KEY => $sessionAPI,);
+        } 
         
         return  $response;
     }
@@ -49,7 +63,7 @@ class WisemblyAPIConnection
         
     }    
     
-   private static function createUserFromResponse(Httpful\Response $response)
+   private static function createUserFromResponse(\Httpful\Response $response)
    {
 
         $user = false;
@@ -67,6 +81,21 @@ class WisemblyAPIConnection
        
         return $user;
    }
+   
+    private static function createSessionAPI(\Httpful\Response $response)
+    {
+        $sessionAPI = false;
+        
+        try{
+            $token = $response->body->success->data->token;
+            
+        }catch(Exception $e){}
+        
+        $sessionAPI = new \MDegayon\WiseAPI\SessionAPI
+                                    ($response->body->success->data->token);
+        
+        return $sessionAPI;
+    }
 }
 
 ?>
