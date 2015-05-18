50c56acd39a1659b958c49590108bcb5ef946a3c
Laura <Laura@Laura-Portatil>
2015-05-18 02:04:18 +0200

    Interface for the "commande pattern" (kinda) created

---
+++ app/Command/CommandInterface.php
@@ -0,0 +1,17 @@
+<?php
+namespace MDegayon\Command;
+/*
+ * Interface for application commands
+ */
+
+/**
+ *
+ * @author Miguel Degayon
+ */
+interface CommandInterface
+{
+    
+    public function execute();
+}
+
+?>
