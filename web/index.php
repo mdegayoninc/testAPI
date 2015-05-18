f419001fd0f3b81f2eaa569a3692e37b24958fae
Laura <Laura@Laura-Portatil>
2015-05-19 00:42:00 +0200

    SessionServiceProvider registered. The class ain't ready yet to have a session cache

--- web/index.php
+++ web/index.php
@@ -3,18 +3,25 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use MDegayon\Controllers\StreamController as StreamController;
 use MDegayon\Controllers\ConnectionLogController as ConnectionLogController;
+use MDegayon\Cache\CacheInterface as CacheInterface;
+use MDegayon\Cache\SessionCache as SessionCache;
 
 
 $app = new Silex\Application();
 
+////Set cache 
+//$app['cache'] = SessionCache::getInstance();
+
 $app->mount('/', new StreamController());
 $app->mount('/connections', new ConnectionLogController());
 
+$app->register(new Silex\Provider\SessionServiceProvider());
 
 $app->register(new Silex\Provider\TwigServiceProvider(), 
                 array( 'twig.path' => __DIR__.'/../app/Views',
                 ));
 
 $app->run();
+
 
 ?>
