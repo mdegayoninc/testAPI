5a7cd0a1fa9e17a844c181ade60114b34f2ef4c4
Laura <Laura@Laura-Portatil>
2015-05-18 23:05:25 +0200

    Deleted some empty lines

--- web/index.php
+++ web/index.php
@@ -1,7 +1,6 @@
 <?php 
 require_once __DIR__.'/../vendor/autoload.php';
 
-
 use MDegayon\Controllers\StreamController as StreamController;
 use MDegayon\Controllers\ConnectionLogController as ConnectionLogController;
 
