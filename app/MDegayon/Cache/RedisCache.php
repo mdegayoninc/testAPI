8086a1dfa1c80e512b64fbf919d480d7ea6bc73f
Laura <Laura@Laura-Portatil>
2015-05-19 00:40:30 +0200

    Just created the class for the Redis implementation of Cacheinterface

---
+++ app/MDegayon/Cache/RedisCache.php
@@ -0,0 +1,22 @@
+<?php
+namespace MDegayon\Cache;
+
+/**
+ * Description of RedisCache
+ *
+ * @author Laura
+ */
+class RedisCache implements \MDegayon\Cache\CacheInterface{
+    
+    public function get($key) 
+    {
+        
+    }
+
+    public function set($key, $value) 
+    {
+        
+    }    
+}
+
+?>
