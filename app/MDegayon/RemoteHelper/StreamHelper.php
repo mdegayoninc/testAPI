d73e32d0d749457b59d32e18b87a35c373d28ee8
Laura <Laura@Laura-Portatil>
2015-05-18 03:09:19 +0200

    Add class for StreamHelper

---
+++ app/MDegayon/RemoteHelper/StreamHelper.php
@@ -0,0 +1,15 @@
+<?php
+namespace MDegayon\RemoteHelper;
+
+
+/**
+ * Used to access to API functions related with Wisembly Streams
+ *
+ * @author Miguel Degayon
+ */
+class StreamHelper 
+{
+   
+}
+
+?>
