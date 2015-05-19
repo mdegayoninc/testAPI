00c35836d9a0759d3e53a637a97a7747adde8259
Laura <Laura@Laura-Portatil>
2015-05-19 22:40:33 +0200

    Added getters, setters and singleton pattern

--- app/MDegayon/Cache/RedisCache.php
+++ app/MDegayon/Cache/RedisCache.php
@@ -8,14 +8,44 @@ namespace MDegayon\Cache;
  */
 class RedisCache implements \MDegayon\Cache\CacheInterface{
     
+    
+    
+    private static $instance;
+    private $redis = false;
+    
+    
+    public static function getInstance()
+    {
+       if (  !self::$instance instanceof self)
+       {
+          self::$instance = new self;
+       }
+       return self::$instance;
+    }
+    
+    public function init(Session $session)
+    {
+        $this->session = $session;
+    }    
+    
     public function get($key) 
     {
         
+        if($this->redis){
+            
+            return $this->redis->get($key, null);
+        }else{
+            throw new \Exception('Uninitialized Cache');
+        }
     }
 
     public function set($key, $value) 
     {
-        
+        if($this->redis){
+            $this->redis->set($key, $value);
+        }else{
+            throw new \Exception('Uninitialized Cache');
+        }      
     }    
 }
 
