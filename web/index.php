a568b9e7ddc6b400b256da90b005faf9a5e77ee0
Laura <Laura@Laura-Portatil>
2015-05-19 23:51:12 +0200

    Replacing SessionCache with RedisCache. Added RedisCache set up

--- web/index.php
+++ web/index.php
@@ -5,24 +5,37 @@ use MDegayon\Controllers\StreamController as StreamController;
 use MDegayon\Controllers\ConnectionLogController as ConnectionLogController;
 use MDegayon\Cache\CacheInterface as CacheInterface;
 use MDegayon\Cache\SessionCache as SessionCache;
+use MDegayon\Cache\RedisCache as RedisCache;
 
 
 $app = new Silex\Application();
 
-
+//Binding routes with controllers
 $app->mount('/', new StreamController());
 $app->mount('/connections', new ConnectionLogController());
 
+
+//Registering session, templating and serialization providers
 $app->register(new Silex\Provider\SessionServiceProvider());
 
-$app->register(new Silex\Provider\TwigServiceProvider(), 
-                array( 'twig.path' => __DIR__.'/../app/Views',
-                ));
+$app->register(new Silex\Provider\SerializerServiceProvider());
 
-////Set up cache 
+$app->register(new Silex\Provider\TwigServiceProvider(), 
+                array( 'twig.path' => __DIR__.'/../app/Views',));
+
+// Set up cache (RedisCache)
+Predis\Autoloader::register();
+
+$cache = RedisCache::getInstance();
+$cache->init(new Predis\Client(), $app['serializer']);
+$app['cache'] = $cache;
+
+// Set up cache (SessionCache)
+/*
 $cache = SessionCache::getInstance();
 $cache->init($app['session']);
 $app['cache'] = $cache;
+*/
 
 //Set up stats
 $stats = MDegayon\WiseAPI\APIStats::getInstance();
