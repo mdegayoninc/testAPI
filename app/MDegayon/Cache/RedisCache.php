57d2838cd7eaac1e3eb26d84081f444292ec1716
Laura <Laura@Laura-Portatil>
2015-05-20 03:02:11 +0200

    Silex serializer isn't as powerful as I thought. Corrections added to unserialize method

--- app/MDegayon/Cache/RedisCache.php
+++ app/MDegayon/Cache/RedisCache.php
@@ -4,7 +4,7 @@ namespace MDegayon\Cache;
 use Symfony\Component\Serializer\Serializer;
 use Predis\Client;
 /**
- * Description of RedisCache
+ *  An application cache system using Regis
  *
  * @author Laura
  */
@@ -37,7 +37,7 @@ class RedisCache implements \MDegayon\Cache\CacheInterface
         
         if($this->redis && $this->serializer){
             
-            $value = $this->redis->get($key, null);
+            $value = $this->redis->get($key);
             
             //Check if $value is a serialized object. If so, deserialize it 
             
@@ -48,42 +48,67 @@ class RedisCache implements \MDegayon\Cache\CacheInterface
         }else{
             throw new \Exception('Uninitialized Cache');
         }
+        
+        return $value;
     }
 
     public function set($key, $value) 
     {
-         is_object();
-         
         if($this->redis && $this->serializer){
             
-            $this->redis->set($key, $value);
+            if(is_object($value)){
+                 
+                $this->redis->set($key, $this->serializeObject($value)); 
+                
+            }else{
+                
+                $this->redis->set($key, $value);      
+            }
         }else{
             throw new \Exception('Uninitialized Cache');
         }      
     }  
     
-    private function serializeObject($object){
+    private function serializeObject($object)
+    {
                 
         return $this->serializer->serialize(
-                                array(  RedisCache::SERIALIZED_OBJECT_KEY => $object, 
-                                        RedisCache::CLASS_KEY => get_class($object),), 
-                                'json');
+                                    array(  RedisCache::SERIALIZED_OBJECT_KEY => $object, 
+                                            RedisCache::CLASS_KEY => get_class($object),), 
+                                    'json');
     }
     
-    private function deserializeObject($jsonArray){
-                
-//        $jsonArray = json_decode($jsonData, true);
+    private function deserializeObject($jsonArray)
+    {
         
-        return $this->serializer->deserialize
+        $phpObject = $this->serializer->deserialize
                                     (json_encode($jsonArray[RedisCache::SERIALIZED_OBJECT_KEY]), 
                                      $jsonArray[RedisCache::CLASS_KEY], 
                                      'json');
         
+        //TODO : Complex classes like APIStats should have their own serialize/deserialize methods 
+        // (using a Serializable interface for example)
+        if($jsonArray[RedisCache::CLASS_KEY] == 'MDegayon\WiseAPI\APIStats'){
+            
+            $data = array();
+            foreach ($jsonArray[RedisCache::SERIALIZED_OBJECT_KEY]['data'] as $currentData) {
+                
+                $data[] = new  \MDegayon\WiseAPI\APIInfoConnection
+                                    ($currentData['method'], $currentData['date'], 
+                                        $currentData['token'], $currentData['user']);
+                
+            }
+            $phpObject->setData($data);
+        }
+
+        return $phpObject;        
     }
     
-    private function isSerializedObject( $jsonData ){
+    private function isSerializedObject( $jsonData )
+    {
         
-        return  array_key_exists(RedisCache::SERIALIZED_OBJECT_KEY,$jsonData) && 
+        return  is_array($jsonData) &&
+                array_key_exists(RedisCache::SERIALIZED_OBJECT_KEY,$jsonData) && 
                 array_key_exists(RedisCache::CLASS_KEY,$jsonData);
     }
 }
